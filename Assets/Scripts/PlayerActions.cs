8914810eeb9ed2443cccde4d36bdfffdc1af8d83
Anne Truong <anne.truongxx@gmail.com>
2024-08-07 13:47:59 +1000

    Object highlight WIP. Needs to disappear when player moves away.

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -21,17 +21,32 @@ public class PlayerActions : MonoBehaviour
 
         foreach (Collider collider in colliderArray)
         {
-            Debug.Log("Detected collider object: " + collider.gameObject.name);
+            if (collider.TryGetComponent(out interactableObject))
+            {
+                HighlightObject(collider);
+            }
         }
     }
 
     public void InteractWithObject()
     {
-        Debug.Log("Interacting with object");
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, interactDistance))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.white);
+            Debug.Log("Did not hit");    
+        }
     }
 
     public void HighlightObject(Collider col)
     {
+        UIText.text = col.gameObject.GetComponent<InteractableObject>().objectName;
         Debug.Log("Highlighting object");
     }
 
