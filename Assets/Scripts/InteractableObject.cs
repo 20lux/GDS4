282fdba31c23fe2397e8692c06171e5c213cc32d
Anne Truong <anne.truongxx@gmail.com>
2024-08-12 10:54:19 +1000

    Created switch statement based on object type

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 
 public class InteractableObject : MonoBehaviour, IInteractable
 {
-    [SerializeField] enum ObjectType {  None = 0, 
-                                        Console = 1, 
-                                        Key = 2, 
-                                        PickableObject = 3 }
+    public enum ObjectType {    None = 0, 
+                                Console = 1, 
+                                Key = 2, 
+                                PickableObject = 3 }
 
     [Header("Interactable Object Properties")]
     [SerializeField] private string ObjectName;
     [HideInInspector] public string objectName => ObjectName;
     [SerializeField] private GameObject objectToInteractWith;
+    [HideInInspector] public ObjectType type;
 
     public void GetObjectTransform(Transform interactorTransform)
     {
         gameObject.transform.position = interactorTransform.position;  
+    }
+
+    public void ObjectAction()
+    {
+        switch (type)
+        {
+            case ObjectType.None:
+                break;
+            case ObjectType.Console:
+                break;
+            case ObjectType.Key:
+                break;
+            case ObjectType.PickableObject:
+                break;
+        }
     }
 
     public string GetInteractText()
