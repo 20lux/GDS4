8bfe3b6cdbf85b730b498cce657cc9e204e79ccf
Inviszish <imitchell090@gmail.com>
2024-09-13 09:07:23 +1000

    UI Toolkit Files
    The start of the UI system for the computer console, with the UI folder and UI Toolkit folder.

---
+++ Assets/UI/Computer_Functions.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Computer_Functions : MonoBehaviour
+{
+
+    //private void OnEnable()
+    //{
+        //VisualElement root = GetComponent<UIDocument>().rootVisualElement;
+
+        //Button buttonAudio = root.Q<Button>("Audio");
+        //Button buttonShip = root.Q<Button>("Ship");
+        //Button buttonMaintance = root.Q<Button>("Maintance");
+
+        //if (buttonAudio.clicked = true)
+        
+    //}
+}
