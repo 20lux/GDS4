6832dae481946396795ad11807cda32bf8e25540
Inviszish <imitchell090@gmail.com>
2024-09-14 00:43:59 +1000

    Play Video Script and render texture
    Script that can store all the video clips for the cartridges, to be attached to the console playing it. Needs video player object in the scene to reference too and to be connected to the render texture. Contains a videoClipIndex value that needs to be defined by the specific cartridges and then the function playVideo to be used in the interact script.

---
+++ Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class PlayVideo : MonoBehaviour
+{
+    public VideoPlayer player;
+    public VideoClip[] videos;
+    public int videoClipIndex;
+    private double duration;
+    private bool isPlaying;
+
+    private void Start()
+    {
+        player.clip = videos[0];
+        player.Play();
+    }
+
+    public void playVideo()
+    {
+        if (isPlaying == false)
+        {
+            player.clip = videos[videoClipIndex];
+            player.Play();
+            duration = videos[videoClipIndex].length;
+            isPlaying = true;
+        }
+        
+    }
+    private void Update()
+    {
+        if (duration >= 0 && isPlaying == true)
+        {
+            duration -= Time.deltaTime;
+        }
+        else if (duration < 0 && isPlaying == true)
+        {
+            player.clip = videos[0];
+            player.Play();
+            isPlaying = false;
+        }
+    }
+
+}
