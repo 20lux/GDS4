b1fd5c132959e878735ffd65de3ebf6d6d9b73b1
Anne Truong <anne.truongxx@gmail.com>
2024-09-26 13:04:04 +1000

    Added evil monologue to bridge console.

--- Assets/Scripts/BridgeEnding.cs
+++ Assets/Scripts/BridgeEnding.cs
@@ -1,8 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class BridgeEnding : MonoBehaviour
 {
-    
+    public VideoPlayer bridgeVideoPlayer;
+    public VideoClip[] bridgeVideoClips;
+    public AudioSource bridgeAudioSource;
+
+    void Start()
+    {
+        bridgeVideoPlayer = GetComponent<VideoPlayer>();
+        bridgeVideoPlayer.isLooping = true;
+        bridgeVideoPlayer.clip = bridgeVideoClips[0];
+    }
+
+    public void PlayBridgeEnding()
+    {
+        bridgeVideoPlayer.playbackSpeed = 0.1f;
+        bridgeVideoPlayer.clip = bridgeVideoClips[1];
+        bridgeAudioSource.Play();
+    }
 }
