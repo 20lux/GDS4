66c0e14bcfed9afa43913c5c55a2c8f1f0261520
Anne Truong <anne.truongxx@gmail.com>
2024-10-09 14:25:38 +1100

    Updated morse code hints. WIP SOS code hint.

--- Assets/Scripts/DestroyTentacle.cs
+++ Assets/Scripts/DestroyTentacle.cs
@@ -3,12 +3,9 @@ using UnityEngine;
 public class DestroyTentacle : MonoBehaviour
 {
     public GameObject[] tentaclesToDestroy;
-    public ButtonPress airlockDoorButton;
 
     public void OpenAirlock()
     {
-        airlockDoorButton.isActive = true;
-
         for (int i = 0; i < tentaclesToDestroy.Length; i++)
         {
             Destroy(tentaclesToDestroy[i]);
